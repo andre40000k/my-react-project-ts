bac8923c68972712ddd7bd0265751b2986fd283b
Andrii <andre40000k@gmail.com>
2025-01-20 21:44:49 +0200

    added async function wich add date from another services

--- src/component/UserList.tsx
+++ src/component/UserList.tsx
@@ -1,8 +1,25 @@
-import { useSelector } from "react-redux";
-import { RootState } from "../store";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchUsersAsync } from "../slices/userSlice";
+import { RootState, AppDispatch } from "../store";
 
 function UserList() {
+  const dispatch = useDispatch<AppDispatch>();
   const users = useSelector((state: RootState) => state.user.users);
+  const loading = useSelector((state: RootState) => state.user.loading);
+  const error = useSelector((state: RootState) => state.user.error);
+
+  useEffect(() => {
+    dispatch(fetchUsersAsync());
+  }, [dispatch]);
+
+  if (loading) {
+    return <p>Loading users...</p>;
+  }
+
+  if (error) {
+    return <p>Error: {error}</p>;
+  }
 
   return (
     <ul>
--- src/slices/userSlice.ts
+++ src/slices/userSlice.ts
@@ -3,11 +3,31 @@ import User from "../interface/IUser";
 
 interface UserState {
   users: User[];
+  loading: boolean;
+  error: string | null;
 }
 
 const initialState: UserState = {
   users: [],
+  loading: false,
+  error: null,
 };
+
+export const fetchUsersAsync = createAsyncThunk<User[], void, { rejectValue: string }>(
+  "user/fetchUsersAsync",
+  async (_, { rejectWithValue }) => {
+    try {
+      const response = await fetch("https://jsonplaceholder.typicode.com/users");
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      const data: User[] = await response.json();
+      return data.map((user) => ({ id: user.id, name: user.name }));
+    } catch (error) {
+      return rejectWithValue("Failed to load users");
+    }
+  }
+);
 
 export const addUserAsync = createAsyncThunk<User, User>
 ("user/addUserAsync", 
@@ -28,13 +48,20 @@ const userSliceReducer = createSlice({
     builder
       .addCase(addUserAsync.fulfilled, (state, action) => {
         state.users.push(action.payload);
+      })
+      .addCase(fetchUsersAsync.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(fetchUsersAsync.fulfilled, (state, action) => {
+        state.loading = false;
+        state.users = action.payload;
+      })
+      .addCase(fetchUsersAsync.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload || "Something went wrong";
       });
-  },
-  // reducers: {
-  //   addUser: (state, action: PayloadAction<User>) => {
-  //     state.users.push(action.payload);
-  //   },
-  // },
+  }
 });
 
 export default userSliceReducer.reducer;
