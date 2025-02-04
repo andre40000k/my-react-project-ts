4d2e6de798dd7fc9d356c999b2441d615c246a33
Andrii <andre40000k@gmail.com>
2025-02-04 20:02:18 +0200

    fix

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    setupFiles: ["./src/setupTests.ts"], // Убедитесь, что путь правильный
-    environment: "jsdom", // Для тестов с React
+    setupFiles: ["./src/setupTests.ts"], 
+    environment: "jsdom", 
   },
 });
